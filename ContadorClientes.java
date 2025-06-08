6d8e35d0ec010c7af23346b859f40d0d8b9d6ca8
Rafaela <rafaela@zasp.com.br>
2025-06-08 20:49:19 -0300

    Adicione ao GitHub

---
+++ ContadorClientes.java
@@ -0,0 +1,9 @@
+public class ContadorClientes {
+    public static void main(String[] args) {
+        System.out.println("Contagem de clientes na loja:");
+
+        for (int cliente = 1; cliente <= 10; cliente++) {
+            System.out.println("Cliente número: " + cliente);
+        }
+    }
+}
